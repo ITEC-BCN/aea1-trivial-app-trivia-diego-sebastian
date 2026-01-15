0ffff09122b486c1d552c7c432aac91272d54b28
SebastianZadomen <sebastian.zadomen.7e8@itb.cat>
2026-01-15 09:59:44 +0100

    MenuScreen Scaffold Progress

--- app/src/main/java/com/example/trivialapp_base/MainActivity.kt
+++ app/src/main/java/com/example/trivialapp_base/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,7 +12,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.trivialapp_base.ui.theme.TrivialAPP_BaseTheme
+import com.example.trivialapp_base.view.MenuScreen
+import com.example.trivialapp_base.viewmodel.GameViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,14 +27,22 @@ class MainActivity : ComponentActivity() {
             TrivialAPP_BaseTheme {
                 // Controlador de navegación
 
-
-
                 // Instanciamos el ViewModel una vez
-
-
+                 val viewModel: GameViewModel by viewModels()
 
                 // Definición de rutas y navegación
+                val navigationController = rememberNavController()
+              /*  NavHost(
+                    navController = navigationController,
+                    startDestination = Routes.ScreenMenu.route
+                ) {
+                    composable(Routes.ScreenMenu.route) { ScreenMenu1(navigationController) }
+                    composable(Routes.ScreenGame.route) { ScreenGame2(navigationController) }
+                    composable(Routes.ScreenResult.route) { ScreenResult3(navigationController) }
+                    composable(Routes.ScreenSplash.route) { ScreenSplash4(navigationController) }
 
+                }*/
+                MenuScreen(navigationController, viewModel)
 
 
             }
--- app/src/main/java/com/example/trivialapp_base/view/Screen_menu.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_menu.kt
@@ -1,10 +1,51 @@
 package com.example.trivialapp_base.view
 
+import android.content.res.Resources
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.trivialapp_base.R
 import com.example.trivialapp_base.viewmodel.GameViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MenuScreen(navController: NavController, viewModel: GameViewModel) {
+    Scaffold( // Slot 1: Barra superior
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("Trivial") },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = colorResource(id = R.color.FondoToolBar)
+                )
+            )
+        }
+    ){innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(16.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        )
+        {
+
+
+
+        }
+
+    }
 
 }
