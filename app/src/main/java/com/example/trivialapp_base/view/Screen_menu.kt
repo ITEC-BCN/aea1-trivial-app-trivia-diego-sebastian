40cbcc2fd47fbfc6dc108183c7402b73f83eab8d
SebastianZadomen <sebastian.zadomen.7e8@itb.cat>
2026-01-16 06:25:24 +0100

    add ScreenMenuCode and resources

--- app/src/main/java/com/example/trivialapp_base/view/Screen_menu.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_menu.kt
@@ -1,51 +1,74 @@
 package com.example.trivialapp_base.view
 
 import android.content.res.Resources
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
 import com.example.trivialapp_base.R
 import com.example.trivialapp_base.viewmodel.GameViewModel
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MenuScreen(navController: NavController, viewModel: GameViewModel) {
-    Scaffold( // Slot 1: Barra superior
+
+
+    Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
-                title = { Text("Trivial") },
+                title = { Text("Trivial", fontFamily = FontFamily.Monospace) },
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
                     containerColor = colorResource(id = R.color.FondoToolBar)
                 )
             )
         }
-    ){innerPadding ->
-        Column(
+    ) { innerPadding ->
+        ConstraintLayout(
             modifier = Modifier
+                .fillMaxSize()
                 .padding(innerPadding)
-                .padding(16.dp)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(20.dp)
-        )
-        {
+        ) {
+            val (img1Ref,btn1Ref) = createRefs()
 
-
-
+            Image(
+                painter = painterResource(id = R.drawable.baseline_videogame_asset_24),
+                contentDescription = "Start",
+                modifier = Modifier.constrainAs(btn1Ref) {
+                    top.linkTo(parent.top, margin = 100.dp)
+                    start.linkTo(parent.start, margin = 100.dp)
+                    end.linkTo(parent.end)
+                }, alpha = 1f
+            )
         }
 
+        // Fondo fuera del ConstraintLayout (llena toda la pantalla)
+        Image(
+            painter = painterResource(id = R.drawable.fondoposible),
+            contentDescription = "FondoGato",
+            modifier = Modifier.fillMaxSize().alpha(1f)
+        )
     }
-
 }
+
+
