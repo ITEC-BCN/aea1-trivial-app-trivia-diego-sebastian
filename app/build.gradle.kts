5adeba7f22360cb05af65e18f64290530b6c122b
SebastianZadomen <sebastian.zadomen.7e8@itb.cat>
2026-01-16 09:54:54 +0100

    add Files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
