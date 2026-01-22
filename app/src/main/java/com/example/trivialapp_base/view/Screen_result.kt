19f799b009f9ce3847a4019d7e307c60796403bd
Diego <diego.grajeda.7e8@itb.cat>
2026-01-22 09:04:27 +0100

    Colors implemented in Screen_result.kt

--- app/src/main/java/com/example/trivialapp_base/view/Screen_result.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_result.kt
@@ -13,8 +13,11 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,68 +27,87 @@ import androidx.compose.ui.modifier.ModifierLocalReadScope
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
 import com.example.trivialapp_base.Routes
 import com.example.trivialapp_base.viewmodel.GameViewModel
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ResultScreen(navController: NavController, viewModel: GameViewModel) {
     var score = viewModel.puntuacion
     val returnToMenu: () -> Unit
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(15.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-
-    )
-    {
-        Text(
-            text = "Your Score",
-            fontSize = 35.sp,
-            fontWeight = FontWeight.Bold
-        )
-
-        Spacer(modifier = Modifier.height(20.dp))
-        Text(
-            text = "$score pts",
-            fontSize = 48.sp,
-            fontWeight = FontWeight.ExtraBold,
-            color = Color.Magenta
-        )
-
-        Spacer(modifier = Modifier.height(40.dp))
-        Button(
-            onClick = {},
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("Trivial", fontFamily = FontFamily.Monospace) },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = colorResource(id = com.example.trivialapp_base.R.color.FondoToolBar)
+                )
+            )
+        }){ innerPadding ->
+        ConstraintLayout(
             modifier = Modifier
-                .height(50.dp),
-            shape = RoundedCornerShape(25)
-        )
-        {
-            Text(
-                text = "Share",
-                fontSize = 18.sp
-            )
-        }
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(colorResource(id = com.example.trivialapp_base.R.color.Fondo))
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(15.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
 
-        Spacer(modifier = Modifier.height(20.dp))
-        Button(
-            onClick = {navController.navigate("ScreenMenu")},
-            modifier = Modifier.height(50.dp),
-            shape = RoundedCornerShape(25)/*,
+                )
+            {
+                Text(
+                    text = "Your Score",
+                    fontSize = 35.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    text = "$score pts",
+                    fontSize = 48.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    color = Color.Magenta
+                )
+
+                Spacer(modifier = Modifier.height(40.dp))
+                Button(
+                    onClick = {},
+                    modifier = Modifier
+                        .height(50.dp),
+                    shape = RoundedCornerShape(25)
+                )
+                {
+                    Text(
+                        text = "Share",
+                        fontSize = 18.sp
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(20.dp))
+                Button(
+                    onClick = {navController.navigate("ScreenMenu")},
+                    modifier = Modifier.height(50.dp),
+                    shape = RoundedCornerShape(25)/*,
             colors = ButtonDefaults.buttonColors(Color.Red)*/
-        )
-        {
-            Text(
-                text = "Return to menu",
-                fontSize = 18.sp
-            )
+                )
+                {
+                    Text(
+                        text = "Return to menu",
+                        fontSize = 18.sp
+                    )
+                }
+            }
         }
     }
 }
